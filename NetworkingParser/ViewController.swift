b73f81775fcda7fe56dda4229919d8961025fbe3
Karl <Karl>
2021-08-05 15:07:58 +0800

    Add result text view

--- NetworkingParser/ViewController.swift
+++ NetworkingParser/ViewController.swift
@@ -8,18 +8,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let activityIndicator = UIActivityIndicatorView(style: .large)
+    @IBOutlet weak var resultTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
-
-    @IBAction func buttonTapped(_ sender: UIButton) {
-        let network = Networking()
-        let activityIndicator = UIActivityIndicatorView(style: .large)
+        setupActivityIndicator()
+        resultTextView.isHidden = true
         activityIndicator.startAnimating()
         
-        self.view.addSubview(activityIndicator)
         
+        let network = Networking()
+        network.fetch(resource: "todos", model: Todo.self) { results in
+            let todos:[Todo] = results as! [Todo]
+            self.resultTextView.isHidden = false
+            self.resultTextView.text = todos.first!.title
+            self.activityIndicator.stopAnimating()
+        }
+    }
+    fileprivate func setupActivityIndicator() {
+
+        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+        activityIndicator.hidesWhenStopped = true
+        self.view.addSubview(activityIndicator)
+
+        let centerXconstraint = activityIndicator.centerXAnchor.constraint(equalTo: self.view.centerXAnchor, constant: 0)
+        let centerYconstraint = activityIndicator.centerYAnchor.constraint(equalTo: self.view.centerYAnchor, constant: 0)
+
+        let constraints = [centerYconstraint, centerXconstraint]
+
+        NSLayoutConstraint.activate(constraints)
+    }
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        let network = Networking()
         network.fetch(resource: "todos", model: Todo.self) { results in
             print(results)
         }
