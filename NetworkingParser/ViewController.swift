c59e4a17702bc725baae1858e1f53e6c235ffb08
Ken Vladimir Mendoza <vladimir.mendoza@opsolutions.biz>
2021-08-05 10:21:02 +0800

    Demonstrate Networking class
    Added button "TAP"

--- NetworkingParser/ViewController.swift
+++ NetworkingParser/ViewController.swift
@@ -14,6 +14,10 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func onTapped(_ sender: Any) {
+        let networking = Networking()
+        networking.fetchPosts()
+    }
+    
 }
 
