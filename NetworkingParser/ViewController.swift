2a85d8e06b28152bcfbd903e3d66a6d54d1c1da3
karlantwan <88015356+karlantwan@users.noreply.github.com>
2021-08-06 09:58:19 +0800

    Refactor networking generics (#5)
    Co-authored-by: Karl <Karl>

--- NetworkingParser/ViewController.swift
+++ NetworkingParser/ViewController.swift
@@ -8,16 +8,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let activityIndicator = UIActivityIndicatorView(style: .large)
+    @IBOutlet weak var resultTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
-
-    @IBAction func buttonTapped(_ sender: UIButton) {
+        setupActivityIndicator()
+        resultTextView.isHidden = true
+        activityIndicator.startAnimating()
+        
+        
         let network = Networking()
-        network.fetchPosts()
-        network.fetchTodos()
+        network.fetch(resource: "todos", model: Todo.self) { results in
+            let todos:[Todo] = results as! [Todo]
+            self.resultTextView.isHidden = false
+            self.resultTextView.text = todos.first!.title
+            self.activityIndicator.stopAnimating()
+        }
+    }
+    fileprivate func setupActivityIndicator() {
+
+        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+        activityIndicator.hidesWhenStopped = true
+        self.view.addSubview(activityIndicator)
+
+        let centerXconstraint = activityIndicator.centerXAnchor.constraint(equalTo: self.view.centerXAnchor, constant: 0)
+        let centerYconstraint = activityIndicator.centerYAnchor.constraint(equalTo: self.view.centerYAnchor, constant: 0)
+
+        let constraints = [centerYconstraint, centerXconstraint]
+
+        NSLayoutConstraint.activate(constraints)
     }
     
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        let network = Networking()
+        network.fetch(resource: "todos", model: Todo.self) { results in
+            print(results)
+        }
+    }
 }
 
